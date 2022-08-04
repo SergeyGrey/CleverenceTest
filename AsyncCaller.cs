b2c7bbc68e8d3297169d0c7833768a369fb215ee
Shadow <God@DESKTOP-JNH1C0Q>
2022-08-04 13:23:13 +0300

    Добавьте файлы проекта.

---
+++ AsyncCaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverenceTest
+{
+    public class AsyncCaller
+    {
+        private EventHandler handler; 
+
+        public AsyncCaller(EventHandler handler)
+        {
+            this.handler = handler;
+        }
+
+        public bool Invoke(int msec, object sender, EventArgs args)
+        {
+            Task task = Task.Factory.StartNew(() => handler(sender, args));
+
+            return task.Wait(msec);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverenceTest
+{
+    public static class Server
+    {
+        private static ReaderWriterLockSlim LockBarrier = new ReaderWriterLockSlim();
+        private static int count = 0;
+
+        public static int GetCount()
+        {
+            LockBarrier.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                LockBarrier.ExitReadLock(); 
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            LockBarrier.EnterWriteLock();
+            try
+            {
+                count += value;
+            }
+            finally 
+            {
+                LockBarrier.ExitWriteLock();
+            }
+        }
+    }
+}
